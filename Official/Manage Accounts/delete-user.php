3b0f0b69514ce121edfeedab1d8571f32b907649
Janinyuh <74756627+Janinyuh@users.noreply.github.com>
2022-01-24 17:18:27 +0800

    Update delete-user.php

--- Official/Manage Accounts/delete-user.php
+++ Official/Manage Accounts/delete-user.php
@@ -1,8 +1,8 @@
 <?php 
 require_once '../Connection/db_conn.php';
 
-	$sql = "DELETE FROM `users` WHERE `userID` = '$_REQUEST[userID]'";
+	$sql = "DELETE FROM users WHERE userID = '".$_GET['userID']."'";
 	$result = mysqli_query($conn, $sql);
-	header("location:Maccounts.php");
+	header("location: Maccounts.php");   //
 
 ?>
