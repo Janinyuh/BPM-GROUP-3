574233f544ba6921e7aa62de2f916c7615365e50
laevad <aldave.tabian@ustp.edu.ph>
2022-01-24 10:39:49 +0800

    config remote sql

--- Connection/db_conn.php
+++ Connection/db_conn.php
@@ -1,9 +1,15 @@
 <?php
+//development
+//$sname= "localhost";
+//$unmae= "root";
+//$password = "";
+//$db_name = "db_bpm2";
 
-$sname= "localhost";
-$unmae= "root";
-$password = "";
-$db_name = "db_bpm2";
+//remote connection
+$sname= "remotemysql.com";
+$unmae= "KWi8ARojlD";
+$password = "wGogCjcbXP";
+$db_name = "KWi8ARojlD";
 
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 
