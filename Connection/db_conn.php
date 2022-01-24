65e1ca197d0ef7f7d653468cf6b37a7e29bb0ecb
laevad <aldave.tabian@ustp.edu.ph>
2022-01-24 12:27:47 +0800

    revert

--- Connection/db_conn.php
+++
@@ -1,9 +0,0 @@
-
-//remote connection
-$sname= "remotemysql.com";
-$unmae= "KWi8ARojlD";
-$password = "wGogCjcbXP";
-$db_name = "KWi8ARojlD";
-
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
-
--- Food pack/delete-foodpack.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require_once '../Connection/db_conn.php';
-
-	$sql = "DELETE FROM `foodpacks` WHERE `fpackID` = '$_REQUEST[fpackID]'";
-	$result = mysqli_query($conn, $sql);
-	header("location:Food pack.php");
-
-?>
